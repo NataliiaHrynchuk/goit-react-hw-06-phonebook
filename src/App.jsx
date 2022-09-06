841035665dc9c9846d96e707ecfbec112f411fd5
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-09-06 17:53:27 +0300

    add localStoradge

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,19 @@ class App extends Component {
       this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }))
-  }
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    this.setState({contacts: parsedContacts});
+  };
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  };
 
 
   render() {
