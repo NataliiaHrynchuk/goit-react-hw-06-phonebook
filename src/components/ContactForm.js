5cf066cda38d4ee86b63d62abce61a4923e3f7d3
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-11-02 19:04:16 +0200

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from 'prop-types';
 import { useState } from "react";
 import {Box} from './Box.styled';
 import {Label} from './Label.styled';
@@ -7,12 +6,15 @@ import {Text} from './Text.styled';
 import { Button } from './Button.styled';
 import { useDispatch } from "react-redux";
 import { addContact } from "redux/contactsSlice";
+import { useSelector } from "react-redux";
+import { getContact } from 'redux/selectors';
 
 
 export default function ContactForm() {
     const dispatch = useDispatch();
     const [name, setName] = useState('');
     const [number, setNumber] = useState('');
+    const contacts = useSelector(getContact);
     
 
     const handleChange = event => {
@@ -31,28 +33,37 @@ export default function ContactForm() {
     
     const handleSubmit = event => {
         event.preventDefault();
-        const contact = {name, number};
-        // onSubmit(contact);
-        dispatch(addContact(contact));
-        setName(' ');
-        setNumber(' ');
+        const newContact = { name, number };
+        const normalizedName = name.toLowerCase();
+        const repeatedName = contacts.find(contact => contact.name.toLowerCase() === normalizedName);
+        const repeatedNumber = contacts.find(contact => contact.number === number);
+            
+        if (repeatedName) {
+            alert(`${name} is already in contacts.`);
+        } else if (repeatedNumber) {
+            alert(`${number} is already in contacts.`);
+        } else {
+            dispatch(addContact(newContact));
+        };
+            setName(' ');
+            setNumber(' ');
         
-    }
+        }
 
         return (
-            <Box 
+            <Box
                 as="form"
                 border="0.5px solid rgb(112, 111, 111)"
                 display="inline-block"
-                w="200px" 
+                w="200px"
                 pad="20px 250px 20px 20px"
                 onSubmit={handleSubmit}
-                >
+            >
                 <Label htmlFor="id-name">
                     Name </Label>
-                <Text 
+                <Text
                     type="text"
-                    name="name" 
+                    name="name"
                     id="id-name"
                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -61,31 +72,25 @@ export default function ContactForm() {
                     mt="10px"
                     mb="10px"
                     onChange={handleChange}
-                    />
+                />
                 
                 <Label htmlFor="id-number">
                     Number </Label>
                 <Text
-                        type="tel"
-                        name="number"
-                        id="id-numder"
-                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                        required
-                        value={number}
-                        onChange={handleChange}
-                        mt="10px"
-                        mb="10px"
-                        mr="120px"/>
+                    type="tel"
+                    name="number"
+                    id="id-numder"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                    value={number}
+                    onChange={handleChange}
+                    mt="10px"
+                    mb="10px"
+                    mr="120px" />
                 
                 <Button type="submit">Add contact</Button>
             </Box>
 
         )
-};
-
-ContactForm.propTypes = {
-    onSubmit: PropTypes.func,
-};
-
-
+    };
