fe769a8d4a56a571399a0164dc4d8f81e0cfba47
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-11-03 13:37:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,6 @@ import  {Filter}  from './components/Filter';
 
 
 export default function App() {
-  // const [contacts, setContacts] = useState(
-  //   JSON.parse(localStorage.getItem('contacts')) ?? []
-  // );
-  
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-  
-
   return (
       <Box pad="20px">
         <h1>Phonebook</h1>
